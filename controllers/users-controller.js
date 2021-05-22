54a95ccc66f228f9a157b41567d21aaf070ce83d
Jessy CHARLOT <50245217+Jessy-DC@users.noreply.github.com>
2021-05-22 21:01:40 +0200

    Add POST for Users

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -1,27 +1,110 @@
-let DUMMY_USERS = [
-    {
-        id: 1,
-        name: 'Jessy',
-        games: {
-            id: 1,
-            title: 'Mass Effect'
-        }
-    },
-    {
-        id: 2,
-        name: 'James',
-        games: {
-            id: 2,
-            title: 'Mario'
-
-        }
-    }
-]
+const { validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
+const HttpError = require('../models/http-error');
 
 const getUsers = async (req, res, next) => {
-    res.json({users: DUMMY_USERS});
+    let users;
+    let projection = {
+        username: 1,
+        games: 1
+    }
+
+    try {
+        users = await User.find({}, projection);
+    } catch (err) {
+        const error = new HttpError(
+            'Fetching users failed, please try again later.',
+            500
+        );
+        return next(error);
+    }
+
+    res.json({users: users.map(user => user.toObject({getters: true}))});
+}
+
+const signup = async (req, res, next) => {
+    const errors = validationResult(req);
+
+    const {username, email, password} = req.body;
+
+    if (!errors.isEmpty()) {
+        console.log(errors);
+        return next(new HttpError(
+            'Invalid inputs passed, please, check your data',
+            422
+        ))
+    }
+
+    let existingUser;
+
+    try {
+        existingUser = await User.findOne({email: email});
+    } catch (err) {
+        const error = new HttpError(
+            'Signing up failed, please try again later',
+            500
+        );
+        return next(error);
+    }
+
+    if (existingUser) {
+        const error = new HttpError(
+            'User exists already, please login instead',
+            422
+        )
+        return next(error);
+    }
+
+    let hashedPassword;
+
+    try {
+        hashedPassword = await bcrypt.hash(password, 12);
+    } catch (err) {
+        const error = new HttpError(
+            'Could not create user, please try again',
+            500
+        );
+        return next(error);
+    }
+
+    let createdUser = new User({
+        username,
+        email,
+        password: hashedPassword,
+        admin: 0,
+        games: []
+    });
+
+    try {
+        await createdUser.save();
+    } catch (err) {
+        const error = new HttpError(
+            'Signing up failed, please try again.',
+            500
+        );
+        return next(error);
+    }
+
+    let token;
+    try {
+        token = jwt.sign(
+            {userId: createdUser.id, email: createdUser.email},
+            process.env.JWT_KEY,
+            {expiresIn: '1h'}
+        );
+    } catch (err) {
+        const error = new HttpError(
+            'Signing up failed, please try again later.',
+            500
+        );
+        return next(error);
+    }
+
+    res.status(201).json({userId: createdUser.id, email: createdUser.email, token: token});
 }
 
 exports.getUsers = getUsers;
+exports.signup = signup;
--- routes/users-routes.js
+++ routes/users-routes.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const { check } = require('express-validator');
 
 const usersController = require('../controllers/users-controller')
 
 const router = express.Router();
 
 router.get('/', usersController.getUsers);
+
+router.post('/signup',
+    [
+        check('username')
+            .not()
+            .isEmpty(),
+        check('email')
+            .normalizeEmail()
+            .isEmail().withMessage('Email must be valid'),
+        check('password')
+            .isLength({min: 7})
+    ], usersController.signup);
 
 module.exports = router;
